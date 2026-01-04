b7a67ddb8d3adc85ea65f3f416e8b67540f3cd84
codehunt022 <arunnath055@gmail.com>
2026-01-05 01:43:39 +0530

    layout dropdown shadcn avatar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div >
+      <Button className='flex min-h-screen home-wrapper'>
+        HOME
+      </Button>
+      </div>
+  )
+}
+
+export default Home
---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+
+import Link from 'next/link';
+import Image from 'next/image';
+import NavItems from '@/components/NavItems';
+import  UserDropDown  from '@/components/UserDropdown';
+
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 header'>
+        <div className='container header-wrapper'>
+            <Link href="/">
+      <Image src="/assets/logo.png" alt="Signalist Logo" width={140} height={32} className="h-8  w-auto cursor-pointer" />
+      </Link>
+      <nav className='hidden sm:block'>
+          <NavItems />
+      </nav>
+      <UserDropDown/>
+        </div>
+      
+    </header>
+  )
+}
+
+export default Header
